356739d1addd0a6135abb39845084ad0dc28295b
John7514 <jjoaovictorpercussao@gmail.com>
2025-06-07 18:52:40 -0300

    Meu Segundo Commit

---
+++ Tela de Login/script.js
@@ -0,0 +1,7 @@
+// script.js
+function handleReset(event) {
+  event.preventDefault();
+  alert("Um link de recuperação foi enviado para o seu e-mail!");
+  // Simulação de envio e retorno à tela de login
+  window.location.href = "index.html";
+}
